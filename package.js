c06af74b0bfeb044b26b9af0c503dc7ce9c3c031
Rahimi542 <fwdwardak@gmail.com>
2025-04-27 12:28:31 +0430

    Add files via upload

---
+++ package.js
@@ -0,0 +1,2 @@
+﻿// ------------------- package.json ------------------- { "name": "webrtc-private-viewer", "version": "1.0.0", "main": "server.js", "scripts": { "start": "node server.js" }, "dependencies": { "express": "^4.18.2", "socket.io": "^4.7.2" } }
+
---
+++ server.js
@@ -0,0 +1,10 @@
+﻿// ------------------- server.js ------------------- const express = require("express"); const http = require("http"); const { Server } = require("socket.io");
+
+const app = express(); const server = http.createServer(app); const io = new Server(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => { socket.on("join", (room) => { socket.join(room); socket.to(room).emit("joined"); });
+
+socket.on("signal", (data) => { socket.to(data.room).emit("signal", data.signal); }); });
+
+server.listen(3000, () => { console.log("Signaling server running on port 3000"); });
+
